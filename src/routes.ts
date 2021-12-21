8a0945d7fa4ba0e3daf42feee52785efc85bf292
PedroGont <pedrotolentino2002@gmail.com>
2021-12-21 03:10:51 -0300

    Commit3

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
+import { GetAllCategoryController } from "./controllers/GetAllCategoriesController";
 
 
 const Routes = Router()
 
+/**
+ * C - Create
+ * R - Read
+ * U - Update
+ * D - Delete
+ */
+
 Routes.post("/categories", new CreateCategoryController().handle)
+Routes.get("/categories", new GetAllCategoryController().handle)
+Routes.delete("/categories/:id", new DeleteCategoryController().handle )
 
 export {Routes}
